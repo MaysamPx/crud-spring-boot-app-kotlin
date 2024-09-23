d1de9a50c7e7a3d464f387cd6d4e189050fccca9
maysampasdari <pasdari.meysam@gmail.com>
2024-09-24 00:01:11 +0200

    Initial commit - Kotlin CRUD app

---
+++ src/main/kotlin/com/kotlin/kotlincrudapp/KotlinCrudAppApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.kotlincrudapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCrudAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCrudAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/kotlincrudapp/KotlinCrudAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.kotlincrudapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCrudAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
