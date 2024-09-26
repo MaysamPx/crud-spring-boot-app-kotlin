77ee1739c6b49021e3a3f0f87f77211c9a9a6d20
MaysamPx <pasdari.meysam@gmail.com>
2024-09-26 16:12:28 +0200

    Updated SQL schema (adapted with PostgreSQL dialect)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,18 @@
--- public.items definition
-
--- Drop table
-
--- DROP TABLE public.items;
+DROP TABLE IF EXISTS public.orders CASCADE;
+DROP TABLE IF EXISTS public.items CASCADE;
 
 CREATE TABLE public.items (
-                              id uuid DEFAULT gen_random_uuid() NOT NULL,
-                              "name" varchar NOT NULL,
-                              price numeric NOT NULL,
-                              "type" varchar NOT NULL,
-                              CONSTRAINT items_pk PRIMARY KEY (id)
+      id uuid DEFAULT gen_random_uuid() NOT NULL,
+      "name" varchar NOT NULL,
+      price numeric NOT NULL,
+      "type" varchar NOT NULL,
+      CONSTRAINT items_pk PRIMARY KEY (id)
 );
 
-
--- public.orders definition
-
--- Drop table
-
--- DROP TABLE public.orders;
-
 CREATE TABLE public.orders (
-                               id uuid DEFAULT gen_random_uuid() NULL,
-                               itemid uuid NULL,
-                               price numeric NULL,
-                               amount numeric NOT NULL,
-                               CONSTRAINT orders_item_fk FOREIGN KEY (id) REFERENCES public.items(id) ON DELETE CASCADE ON UPDATE CASCADE
+       id uuid DEFAULT gen_random_uuid() NULL,
+       item_id uuid NULL,
+       price numeric NULL,
+       amount numeric NOT NULL,
+       CONSTRAINT orders_item_fk FOREIGN KEY (item_id) REFERENCES public.items(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
